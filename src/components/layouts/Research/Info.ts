5b71dff7859a16b4096925bc7337d5b9e46153b2
shwg8986 <sho8986go@gmail.com>
2023-04-27 15:13:23 +0900

    Publicationsの修正

--- src/components/layouts/Research/Info.ts
+++ src/components/layouts/Research/Info.ts
@@ -49,8 +49,8 @@ function createData(
 
 
 export const Publications = [
-    createData('S.Shimada, Y.Ikei, N.Nishiuchi, and V.Yem. "Study of cybersickness prediction in real time using eye tracking data", IEEE Conference on Virtual Reality and 3D User Interfaces (IEEE VR), March 25-29, 2023, Shanghai in China. [Accepted with peer review] '),
-    createData('S.Shimada, Y.Ojima, Y.Kikuchi, Y.Ikei, and V.Yem. "Comparative evaluation of joystick and leaning-based locomotion in immersive telepresence with body motion feedback", Association for Computing Machinery, Augmented Humans (ACM AHs), March 12-14, 2023, Glasgow in United Kingdom. [Accepted with peer review] '),
+    createData('S.Shimada, Y.Ikei, N.Nishiuchi, and V.Yem. "Study of cybersickness prediction in real time using eye tracking data", IEEE Conference on Virtual Reality and 3D User Interfaces (IEEE VR), March 25-29, 2023, Shanghai in China. [With peer review] '),
+    createData('S.Shimada, Y.Ojima, Y.Kikuchi, Y.Ikei, and V.Yem. "Comparative evaluation of joystick and leaning-based locomotion in immersive telepresence with body motion feedback", Association for Computing Machinery, Augmented Humans (ACM AHs), March 12-14, 2023, Glasgow in United Kingdom. [With peer review] '),
     createData('島田匠悟, 西内信之 "VR空間におけるドラッグ&ドロップ操作のインタフェース評価に関する研究", 日本人間工学会 第63回大会, July 30-31, 2022, Hiroshima in Japan. [Without peer review] '),
 ];
 
