7071b8a3f908f656416d0f176223341445362768
shwg8986 <sho8986go@gmail.com>
2023-06-23 00:00:05 +0900

    Experienceの修正

--- src/components/layouts/Experience/Experience.tsx
+++ src/components/layouts/Experience/Experience.tsx
@@ -88,7 +88,7 @@ export const Experience = () => {
                                     aria-controls="panel3a-content"
                                     id="panel3a-header"
                                 >
-                                    <Typography>Getting ready... from 2024</Typography>
+                                    <Typography>Next... from 2024</Typography>
                                 </AccordionSummary>
                             </Accordion>
                         </div>
