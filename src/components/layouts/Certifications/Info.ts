560896ec1a518c1e4e35d5822a32cea87d3d4e16
shwg8986 <sho8986go@gmail.com>
2023-06-22 23:45:40 +0900

    資格の編集

--- src/components/layouts/Certifications/Info.ts
+++ src/components/layouts/Certifications/Info.ts
@@ -2,10 +2,10 @@ function createData(
     Dates: string,
     schools: string,
 ) {
-    return {Dates, schools};
+    return { Dates, schools };
 }
 
 export const CertificationsInfo = [
     createData('2021年2月', "TOEIC L&R Total Score 755"),
-    createData('2021年12月', "基本情報技術者（Fundamental Information Technology Engineer Examination）"),
+    createData('2021年12月', "基本情報技術者（FE）"),
 ];
