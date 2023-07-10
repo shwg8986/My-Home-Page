b4ba76b30d0359a99d32f24237c6c6046c147317
shwg8986 <sho8986go@gmail.com>
2023-07-11 00:36:00 +0900

    Experienceの修正

--- src/components/layouts/Experience/Info.ts
+++ src/components/layouts/Experience/Info.ts
@@ -9,31 +9,37 @@ export type ExperienceType = {
 export const ExperienceInfo = [
     {
         "title": "株式会社Techouse",
-        "days": "2022年6月〜10月",
+        "days": "2022/6〜2022/10",
         "content": "TypeScript（React）を使ったフロントエンドのエンジニアインターンをしていました。",
         "URL": "https://www.techouse.jp/",
     },
     {
         "title": "研究補助 臨時職員",
-        "days": "2022年6月〜7月 ",
+        "days": "2022/6〜2022/7 ",
         "content": "東京都立大学 情報科学域 ヤェムヴィボル助教の研究補助アルバイトをしていました。",
         "content2": "業務内容：ロボットアームを用いた遠隔作業システムの開発",
     },
     {
         "title": "リサーチアシスタント（RA）: 第一次",
-        "days": "2022年8月〜2023年2月",
+        "days": "2022/8〜2023/2",
         "content": "東京都立大学 情報科学域 ヤェムヴィボル助教のバーチャルリアリティに関する研究開発のアシスタントをしていました。",
         "content2": "業務内容：ロボットアームを用いた遠隔作業システムの開発, VR酔い検出手法の探求",
     },
     {
         "title": "株式会社テレビ東京",
-        "days": "2022年12月",
+        "days": "2022/12",
         "content": "Tech & DX部門の冬季インターンシップに参加しました。",
         "URL": "https://www.tv-tokyo.co.jp/",
     },
     {
         "title": "リサーチアシスタント（RA）: 第ニ次",
-        "days": "2023年5月〜現在",
+        "days": "2023/5〜current",
         "content": "筑波大学 システム情報系 ヤェムヴィボル准教授のバーチャルリアリティに関する研究開発のアシスタントをしています。",
+    },
+    {
+        "title": "Biometrics - intelligent solutions in Poland",
+        "days": "2023/6/26〜2023/7/8",
+        "content": "“International Summer School: Biometrics - intelligent solutiona at Białystok University of Technology, Poland” に参加しました。(The project is implemented within the SPINAKER programme - international intensive programmes run by the polish national agency for academic exchange(NAWA). It provides for the organization of a summer school - “Biometrics - intelligent solutions”.)",
+        "URL": "https://pb.edu.pl/iro/nawa-projects/biometrics-intelligent-solutions/",
     },
 ]
