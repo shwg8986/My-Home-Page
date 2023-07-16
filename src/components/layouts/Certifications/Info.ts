f136882d343fbde527a6e38caed679bb485f64cc
shwg8986 <sho8986go@gmail.com>
2023-07-16 15:43:28 +0900

    資格の変更

--- src/components/layouts/Certifications/Info.ts
+++ src/components/layouts/Certifications/Info.ts
@@ -6,6 +6,7 @@ function createData(
 }
 
 export const CertificationsInfo = [
-    createData('2021年2月', "TOEIC L&R Total Score 755"),
+    createData('2021年2月', "TOEIC L&R Total Score 780"),
     createData('2021年12月', "基本情報技術者, 情報処理推進機構"),
+    createData('2023年7月', "Paiza プログラミングスキルチェック Sランク"),
 ];
