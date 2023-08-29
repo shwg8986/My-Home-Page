ec5204c3532f838e72d4e435a0a73212b1ee37de
shwg8986 <sho8986go@gmail.com>
2023-08-29 19:14:12 +0900

    取得済みの資格の追加

--- src/components/layouts/Certifications/Info.ts
+++ src/components/layouts/Certifications/Info.ts
@@ -7,6 +7,7 @@ function createData(
 
 export const CertificationsInfo = [
     createData('2021年2月', "TOEIC L&R Total Score 780"),
-    createData('2021年12月', "基本情報技術者, 情報処理推進機構"),
+    createData('2021年12月', "基本情報技術者試験, 情報処理推進機構"),
     createData('2023年7月', "Paiza プログラミングスキルチェック Sランク"),
+    createData('2023年8月', "バーチャルリアリティ技術者認定試験 セオリーコース, 日本VR学会"),
 ];
