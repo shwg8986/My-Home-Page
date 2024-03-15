40590801f0c4fee3045b8921729a87a9184fab89
shwg8986 <sho8986go@gmail.com>
2024-03-15 17:30:29 +0800

    経歴の追加

--- src/components/layouts/Experience/Info.ts
+++ src/components/layouts/Experience/Info.ts
@@ -33,13 +33,18 @@ export const ExperienceInfo = [
     },
     {
         "title": "リサーチアシスタント(RA): 第ニ次",
-        "days": "2023/5〜current",
-        "content": "筑波大学 システム情報系 ヤェムヴィボル准教授のバーチャルリアリティに関する研究開発のアシスタントをしています。",
+        "days": "2023/5〜2024/3",
+        "content": "筑波大学 システム情報系 ヤェムヴィボル准教授のバーチャルリアリティに関する研究開発のアシスタントをしていました。",
     },
     {
         "title": "Biometrics - intelligent solutions in Poland",
         "days": "2023/6/26〜2023/7/8",
-        "content": "“International Summer School: Biometrics - intelligent solutions at Białystok University of Technology, Poland” に参加しました。(The project is implemented within the SPINAKER programme - international intensive programmes run by the polish national agency for academic exchange(NAWA). It provides for the organization of a summer school - “Biometrics - intelligent solutions”.)",
+        "content": "I joined “International Summer School: Biometrics - intelligent solutions at Białystok University of Technology, Poland.” (The project was implemented within the SPINAKER programme - international intensive programmes run by the polish national agency for academic exchange(NAWA). It provided for the organization of a summer school - “Biometrics - intelligent solutions”.)",
         "URL": "https://pb.edu.pl/iro/nawa-projects/biometrics-intelligent-solutions/",
+    },
+    {
+        "title": "KDDI株式会社",
+        "days": "2024/4〜current",
+        "content": "Engineer (Full-time).",
     },
 ]
