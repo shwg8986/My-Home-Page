d02e5dac1470d353f39ecd6d6b9c78b3c01f8e43
shwg8986 <sho8986go@gmail.com>
2024-05-27 14:05:39 +0900

    資格の追加

--- src/components/layouts/Certifications/Info.ts
+++ src/components/layouts/Certifications/Info.ts
@@ -11,4 +11,5 @@ export const CertificationsInfo = [
     createData('2023/7', "Paiza プログラミングスキルチェック Sランク"),
     createData('2023/8', "バーチャルリアリティ技術者認定試験 セオリーコース, 日本VR学会"),
     createData('2024/2', "バーチャルリアリティ技術者認定試験 アプリケーションコース, 日本VR学会"),
+    createData('2024/5', "G検定, 一般社団法人日本ディープラーニング協会"),
 ];
