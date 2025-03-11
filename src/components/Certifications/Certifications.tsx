0172e63b2c0eb5b0e63ce368de8773eb4739017c
shwg8986 <sho8986go@gmail.com>
2025-03-11 19:53:54 +0900

    資格の追加

--- src/components/Certifications/Certifications.tsx
+++ src/components/Certifications/Certifications.tsx
@@ -1,114 +1,152 @@
-import * as React from 'react';
-import { Box, Typography, Grid, Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import * as React from "react";
+import {
+  Box,
+  Typography,
+  Grid,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import { CertificationsInfo } from "./Info";
 
-const rows = [...CertificationsInfo]
+const rows = [...CertificationsInfo];
 export const Certifications = () => {
-    return (
-        <Box sx={{
-            width: "100%",
-            p: "50px 0",
-            mb: 8,
-            borderStyle: "solid",
-            borderColor: "rgba(194, 224, 255, 0.08)",
-            borderWidth: "0px 0px thin",
-            backgroundColor: "snow",
-        }}>
-            <Grid sx={{ flexGrow: 1, mb: 5 }} container spacing={3}>
-                <Grid item sx={{
-                    m: "0 9%",
-                }}>
-                    <Typography
-                        component="h1"
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        p: "50px 0",
+        mb: 8,
+        borderStyle: "solid",
+        borderColor: "rgba(194, 224, 255, 0.08)",
+        borderWidth: "0px 0px thin",
+        backgroundColor: "snow",
+      }}
+    >
+      <Grid sx={{ flexGrow: 1, mb: 5 }} container spacing={3}>
+        <Grid
+          item
+          sx={{
+            m: "0 9%",
+          }}
+        >
+          <Typography
+            component="h1"
+            sx={{
+              color: "#ab0f50",
+              fontSize: 30,
+              fontWeight: "Prime",
+              ml: 1.5,
+            }}
+          >
+            Certifications
+          </Typography>
+          <Typography
+            component="h2"
+            sx={{
+              color: "#gray",
+              fontSize: 14,
+              fontWeight: "Light",
+              ml: 6,
+            }}
+          >
+            ー 資格など ー
+          </Typography>
+        </Grid>
+      </Grid>
+      <Container>
+        <Grid container spacing={1}>
+          <Grid
+            item
+            sx={{
+              width: "90%",
+              m: "0 auto",
+            }}
+          >
+            <TableContainer component={Paper}>
+              <Table
+                sx={{
+                  maxWidth: 1000,
+                  backgroundColor: "#fff",
+                  color: "#fff",
+                }}
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      sx={{
+                        color: "#000",
+                        fontWeight: "500",
+                        fontSize: 16,
+                        p: "16px",
+                        "@media screen and (max-width: 420px)": {
+                          fontSize: 14,
+                        },
+                      }}
+                    >
+                      年/月
+                    </TableCell>
+                    <TableCell
+                      align="left"
+                      sx={{
+                        color: "#000",
+                        fontWeight: "500",
+                        fontSize: 16,
+                        "@media screen and (max-width: 420px)": {
+                          fontSize: 14,
+                        },
+                      }}
+                    >
+                      資格/試験
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.Dates}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
                         sx={{
-                            color: "#ab0f50",
-                            fontSize: 30,
-                            fontWeight: "Prime",
-                            ml: 1.5,
-                        }}>
-                        Certifications
-                    </Typography>
-                    <Typography
-                        component="h2"
-                        sx={{
-                            color: "#gray",
+                          fontSize: 16,
+                          fontWeight: "normal",
+                          "@media screen and (max-width: 420px)": {
                             fontSize: 14,
-                            fontWeight: "Light",
-                            ml: 6,
-                        }}>
-                        ー 資格など ー
-                    </Typography>
-                </Grid>
-            </Grid>
-            <Container>
-                <Grid container spacing={1}>
-                    <Grid item sx={{
-                        width: "90%",
-                        m: "0 auto",
-                    }}>
-                        <TableContainer component={Paper}>
-                            <Table sx={{
-                                maxWidth: 1000,
-                                backgroundColor: "#fff",
-                                color: "#fff"
-                            }}
-                                aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell
-                                            sx={{
-                                                color: "#000",
-                                                fontWeight: "500",
-                                                fontSize: 16,
-                                                p: "16px",
-                                                "@media screen and (max-width: 420px)": {
-                                                    fontSize: 14,
-                                                },
-                                            }}>年/月</TableCell>
-                                        <TableCell align="left"
-                                            sx={{
-                                                color: "#000",
-                                                fontWeight: "500",
-                                                fontSize: 16,
-                                                "@media screen and (max-width: 420px)": {
-                                                    fontSize: 14,
-                                                },
-                                            }}>試験</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {rows.map((row) => (
-                                        <TableRow
-                                            key={row.Dates}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row"
-                                                sx={{
-                                                    fontSize: 16,
-                                                    fontWeight: "normal",
-                                                    "@media screen and (max-width: 420px)": {
-                                                        fontSize: 14,
-                                                    },
-                                                }}>
-                                                {row.Dates}
-                                            </TableCell>
-                                            <TableCell align="left"
-                                                sx={{
-                                                    color: "#000",
-                                                    fontSize: 16,
-                                                    fontWeight: "normal",
-                                                    "@media screen and (max-width: 420px)": {
-                                                        fontSize: 14,
-                                                    },
-                                                }}>{row.Certifications}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
-    )
-}
+                          },
+                        }}
+                      >
+                        {row.Dates}
+                      </TableCell>
+                      <TableCell
+                        align="left"
+                        sx={{
+                          color: "#000",
+                          fontSize: 16,
+                          fontWeight: "normal",
+                          "@media screen and (max-width: 420px)": {
+                            fontSize: 14,
+                          },
+                        }}
+                      >
+                        {row.Certifications}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
--- src/components/Certifications/Info.ts
+++ src/components/Certifications/Info.ts
@@ -1,16 +1,30 @@
-function createData(
-    Dates: string,
-    Certifications: string,
-) {
-    return { Dates, Certifications };
+function createData(Dates: string, Certifications: string) {
+  return { Dates, Certifications };
 }
 
 export const CertificationsInfo = [
-    createData('2021/2', "TOEIC L&R Total Score 780"),
-    createData('2021/12', "基本情報技術者試験, 情報処理推進機構"),
-    createData('2023/7', "Paiza プログラミングスキルチェック Sランク"),
-    createData('2023/8', "バーチャルリアリティ技術者認定試験 セオリーコース, 日本VR学会"),
-    createData('2024/2', "バーチャルリアリティ技術者認定試験 アプリケーションコース, 日本VR学会"),
-    createData('2024/5', "G検定, 一般社団法人日本ディープラーニング協会"),
-    createData('2024/6', "第一級陸上特殊無線技士, 公益財団法人 日本無線協会"),
+  createData("2021/2", "TOEIC L&R Total Score 780"),
+  createData("2021/12", "基本情報技術者試験, 情報処理推進機構"),
+  createData("2023/7", "Paiza プログラミングスキルチェック Sランク"),
+  createData(
+    "2023/8",
+    "バーチャルリアリティ技術者認定試験 セオリーコース, 日本VR学会"
+  ),
+  createData(
+    "2024/2",
+    "バーチャルリアリティ技術者認定試験 アプリケーションコース, 日本VR学会"
+  ),
+  createData("2024/5", "G検定, 一般社団法人日本ディープラーニング協会"),
+  createData("2024/6", "第一級陸上特殊無線技士, 公益財団法人 日本無線協会"),
+  createData("2024/9", "SnowPro Core (COF-CO2), Snowflake."),
+  createData(
+    "2024/10",
+    "AWS Certified Cloud Practitioner (COF-CO2), Amazon Web Services (AWS)."
+  ),
+  createData("2024/12", "Registered Scrum Master®, Scrum Inc."),
+  createData("2024/12", "LinuC Level-1 Certification, LPI-Japan."),
+  createData(
+    "2025/3",
+    "AWS Certified Solutions Architect - Associate (SAA-C03), Amazon Web Services (AWS)."
+  ),
 ];
